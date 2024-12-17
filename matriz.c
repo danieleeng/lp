40c28fd59d170f0abca4da4fe5d2c793c98e9d93
danieleeng <daniel.rc@estudante.iftm.edu.br>
2024-12-17 12:31:54 +0100

    Create matriz.c
    Guia de como usar matrizes para a prova.

---
+++ matriz.c
@@ -0,0 +1,16 @@
+// como trabalhar com matrizes
+#define MAX 1000
+
+void funcao(matriz[MAX][MAX]) { 
+    // ...
+}
+
+int main() {
+    int matriz[MAX][MAX]; // definir matriz com tamanho pre-estabelecido
+
+    // ...
+
+    funcao(matriz); // chamar funcao passando so o nome da matriz, sem passar as dimensoes
+
+    return 0;
+}
